62a77cf9122491bebadf5d6fe677d8126a1e15c1
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-03-22 23:01:57 +0100

    feat: :sparkles: new appi resource list/failed
    se le añaden las rutas /list y /failed al api

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from controller.start import Start
+from controller.failed import Failed
+from controller.list import List
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Start, '/')
-# api.add_resource(List, '/list')
-# api.add_resource(Fail, '/fail')
+api.add_resource(List, '/list')
+api.add_resource(Failed, '/failed')
 
 if __name__ == '__main__':
     app.run(debug=True)
