538a54f9a3aa894b0d62a8a1ff99927eef721f83
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-03-27 20:42:40 +0100

    code comments

--- service/date_validator.py
+++ service/date_validator.py
@@ -2,7 +2,8 @@ import datetime
 
 
 def date_validator(date_text):
-    if "birth_date" in date_text.keys():
+    if "birth_date" in date_text.keys():  # birth_date no es un campo obligado en el json
+        # check del formato del valor en la llave "birth_date"
         return datetime.datetime.strptime(date_text["birth_date"], '%Y-%m-%d')
     else:
         return True
--- service/nif_validator.py
+++ service/nif_validator.py
@@ -1,11 +1,13 @@
 def nif_validator(nif):
 
-    letter_list = "TRWAGMYFPDXBNJZSQVHLCKE"
+    letter_list = "TRWAGMYFPDXBNJZSQVHLCKE"  # pull de letras y numeros
     num_list = "1234567890"
 
-    if (len(nif["id"]) == 10):
-        dni = nif["id"][:8]
+    if (len(nif["id"]) == 10):  # check sobre la longitud de id
+        dni = nif["id"][:8]  # variable que conserva los numeros del id
+        # comprobación de que toda la extensión de dni son numeros
         if (len(dni) == len([n for n in dni if n in num_list])):
+            # validación de la letra del id comparando con la pull de letras
             if letter_list[int(dni) % 23] == nif["id"][9].upper():
                 return True
             else:
