ac4ba0fd867ecd231409c6fca38e680c1d7fe6cb
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-03-27 15:13:22 +0100

    remove coment_test

--- test/test_date_validator.py
+++ test/test_date_validator.py
@@ -15,7 +15,6 @@ def if_validate(sample):
 def test_date_validator():
     assert datetime.datetime(1999, 12, 5, 0, 0) == date_validator(
         {"birth_date": "1999-12-05"})
-    # assert datetime.datetime(2005, 12, 19, 0, 0) != date_validator(
-    #     {"birth_date": "19-12-2005"})
+
     assert False == if_validate({"birth_date": "15-12-2005"})
     assert True == if_validate({"birth_date": "2005-12-15"})
