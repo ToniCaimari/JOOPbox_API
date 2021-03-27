92ae10d6f5a88f4d82d392d89db778c5c8ba8836
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-03-27 15:12:13 +0100

    test: test_date_validation if_validate implemented/ negative tests
    Se ha añadido la función if_validate como filtro para verificar si la salida de un test es o no correcta
    
    negative test == Error (not None or False) implemented try/except returning boolean function to solve it

--- test/test_date_validator.py
+++ test/test_date_validator.py
@@ -3,9 +3,19 @@ from service.date_validator import date_validator
 import datetime
 
 
+def if_validate(sample):
+    try:
+        date_validator(sample)
+        return True
+    except:
+        return False
+
+
 @pytest.mark.date_format
 def test_date_validator():
     assert datetime.datetime(1999, 12, 5, 0, 0) == date_validator(
         {"birth_date": "1999-12-05"})
     # assert datetime.datetime(2005, 12, 19, 0, 0) != date_validator(
     #     {"birth_date": "19-12-2005"})
+    assert False == if_validate({"birth_date": "15-12-2005"})
+    assert True == if_validate({"birth_date": "2005-12-15"})
